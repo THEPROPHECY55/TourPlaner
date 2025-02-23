acea4059d7aa48a4bf1c5011b259c0ad77279386
THEPROPHECY55 <radicivo255@gmail.com>
2025-02-23 16:27:24 +0100

    Version 0.2 finished

---
+++ TourPlaner/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace TourPlaner;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Exit_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+    private void About_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Tour Planner Application / SWEN2 PROJECT 2025\nBuild V0.2 - 23.02.2025\n\nDeveloped by Ivo Radic & Wendelin Windhager", "About Tour Planner", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+}
---
+++ TourPlaner/ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TourPlaner.ViewModels;
+
+public class Tour
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    private Tour _selectedTour;
+    public ObservableCollection<Tour> Tours { get; set; }
+
+    public Tour SelectedTour
+    {
+        get => _selectedTour;
+        set
+        {
+            _selectedTour = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public MainViewModel()
+    {
+        Tours = new ObservableCollection<Tour>
+        {
+            new Tour { Name = "Floridsdorf", Description = "Sehr schwere Route." },
+            new Tour { Name = "Brigitenau", Description = "Eine einfache Route." }
+        };
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
